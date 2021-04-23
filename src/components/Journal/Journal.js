b3cdece26fa57f6273f33920555a200777ea23e6
Oussama <oussamus_84@hotmail.com>
2021-04-22 23:24:46 -0400

    Other fixes

--- src/components/Journal/Journal.js
+++ src/components/Journal/Journal.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import "./Journal.css";
 import { Typography, Container, Card, CardContent } from "@material-ui/core";
@@ -26,32 +25,3 @@ function Journal() {
 }
 
 export default Journal;
-=======
-import React from "react";
-import "./Journal.css";
-import { Typography, Container, Card, CardContent } from "@material-ui/core";
-
-function Journal() {
-  return (
-    <>
-      <React.Fragment>
-        <Container>
-          <Typography variant="h3" className="title">
-            Journal Page
-          </Typography>
-          <Card className="caloriecard">
-            <CardContent>
-              <Typography variant="h5" component="h2">
-                Your calories
-              </Typography>
-              <Typography align="center">0 calories</Typography>
-            </CardContent>
-          </Card>
-        </Container>
-      </React.Fragment>
-    </>
-  );
-}
-
-export default Journal;
->>>>>>> e9c051d04b2f1e941f2a4dcf189becfa86da7fd5
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import "./Profile.css";
 import { Typography, Container } from "@material-ui/core";
@@ -18,24 +17,3 @@ function Profile() {
 }
 
 export default Profile;
-=======
-import React from "react";
-import "./Profile.css";
-import { Typography, Container } from "@material-ui/core";
-
-function Profile() {
-  return (
-    <>
-      <React.Fragment>
-        <Container>
-          <Typography variant="h3" className="title">
-            Profile Page
-          </Typography>
-        </Container>
-      </React.Fragment>
-    </>
-  );
-}
-
-export default Profile;
->>>>>>> e9c051d04b2f1e941f2a4dcf189becfa86da7fd5
